05e102658b8ce9021586c2148a85fe9764332dbf
Vladyslav Istomin <vladyslav_istomin@epam.com>
2026-05-20 12:04:08 +0300

    new commit

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
+import datetime as dt
+import pandas as pd
 import random
 import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+month_now = dt.datetime.today().month
+day_now = dt.datetime.today().day
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+birthday_data = pd.read_csv('birthdays.csv').to_dict('records')
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+for birthday in birthday_data:
+    if birthday['month'] == month_now and birthday['day'] == day_now:
+        birthday_name = birthday['name']
+        with open(f'letter_templates/letter_{random.randint(1, 3)}.txt', 'r') as file:
+            file = file.read()
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        replaced_name = file.replace("[NAME]", birthday_name)
+        new_mail = "".join(replaced_name)
+
+        my_email = os.environ.get("MY_EMAIL")
+        my_password = os.environ.get("MY_PASSWORD")
+        with smtplib.SMTP('smtp.ethereal.email', 587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(to_addrs=my_email, msg=f"Subject:Happy Birthday\n\n{new_mail}", from_addr=my_email)
